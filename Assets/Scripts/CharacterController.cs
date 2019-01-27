748d7efb005a571a5b34a620eb99801478568b25
Wulfbane <wulvesgaming@gmail.com>
2019-01-27 15:11:12 -0500

    initial-commit
    The very first push of Antics. Already contains basic player movement, smoothed camera movement, and some early debugging sprites.

---
+++ Assets/Scripts/CharacterController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(CharacterMotor))]
+public class CharacterController : MonoBehaviour {
+
+    [SerializeField]
+    private float speed = 5f;
+
+    [SerializeField]
+    private float sensitivity = 3f;
+
+    private CharacterMotor motor;
+
+	// Use this for initialization
+	void Start () {
+        motor = GetComponent<CharacterMotor>();
+	}
+	    
+	// Update is called once per frame
+	void Update () {
+        // Calculate Movement Velocity as a 2d Vector
+        float _xMov = Input.GetAxisRaw("Horizontal");
+        float _yMov = Input.GetAxisRaw("Vertical");
+
+        Vector2 _movHorizontal = transform.right * _xMov;
+        Vector2 _movLateral = transform.up * _yMov;
+
+        // Final Movement Vector
+        Vector2 _velocity = (_movHorizontal + _movLateral).normalized * speed;
+
+        // Apply Movement
+        motor.Move(_velocity);
+
+	}
+}
---
+++ Assets/Scripts/CharacterMotor.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterMotor : MonoBehaviour {
+
+    public bool invertMouse = false;
+
+    [SerializeField]
+    private Camera cam;
+
+    private Vector2 velocity = Vector2.zero;
+
+    private Rigidbody2D rb;
+
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody2D>();
+	}
+
+    // Get a Movement Vector
+    public void Move(Vector2 _velocity)
+    {
+        velocity = _velocity;
+    }
+	
+    // Run every physics iteration
+    void FixedUpdate ()
+    {
+        PerformMovement();
+    }
+
+    // Perform movement based on velocity variable
+    void PerformMovement()
+    {
+        if(velocity != Vector2.zero)
+        {
+            rb.MovePosition(rb.position + velocity * Time.fixedDeltaTime);
+        }
+    }
+}
